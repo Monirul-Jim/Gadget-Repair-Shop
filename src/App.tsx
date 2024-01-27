051c9f8ef955382648fc613d51239e31f5133b84
unknown <Monirul-Jim>
2024-01-27 21:22:51 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>This is new component</h1>
+      <Button>Something</Button>
+    </>
+  );
+}
+
+export default App;
