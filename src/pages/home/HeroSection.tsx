447f0a1a0f456b1a09bac76d5241770a37cd61d6
unknown <Monirul-Jim>
2024-01-31 21:45:34 +0600

    added framer motion to hero text

--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -8,13 +8,15 @@ const HeroSection = () => {
     visible: {
       opacity: 1,
       transition: {
-        duration: 5,
+        duration: 0.5,
+        staggerChildren: 0.25,
+        delayChildren: 1,
       },
     },
   };
   const introChildren = {
-    hidden: { opacity: 0, y: 500 },
-    visible: { opacity: 1, y: 0 },
+    hidden: { opacity: 0, y: -200 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
   };
   return (
     <Container className="h-[calc(100vh-64px)] grid grid-cols-1 lg:grid-cols-2 place-content-center">
@@ -27,7 +29,10 @@ const HeroSection = () => {
           <br />
           <span>We'll fix it</span>
         </motion.h1>
-        <motion.p className="text-dark-gray max-w-[50ch] mt-10 mb-5 text-lg">
+        <motion.p
+          className="text-dark-gray max-w-[50ch] mt-10 mb-5 text-lg"
+          variants={introChildren}
+        >
           Welcome to{" "}
           <span className="text-primary-foreground font-semibold">iRepair</span>{" "}
           your one-stop place for all kinds of{" "}
@@ -36,7 +41,9 @@ const HeroSection = () => {
           </span>{" "}
           and diagnostics
         </motion.p>
-        <Button>Book a service</Button>
+        <motion.div variants={introChildren}>
+          <Button>Book a service</Button>
+        </motion.div>
       </motion.div>
       <div className="mt-10 w-3/4 lg:w-full">
         <img className="h-[95%] object-contain" src={macbook} alt="" />
