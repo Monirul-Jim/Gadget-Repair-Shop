6140cd59c0e3d419340041881b33c57b17c93b20
unknown <Monirul-Jim>
2024-02-04 23:44:43 +0600

    invalidate tags

--- src/api/admin/service/service.hook.ts
+++ src/api/admin/service/service.hook.ts
@@ -20,6 +20,7 @@ export const useGetServices = () => {
       }));
       return services;
     },
+    // gcTime:1000
   });
   return servicesData;
 };
--- src/pages/admin/ServicesList.tsx
+++ src/pages/admin/ServicesList.tsx
@@ -10,7 +10,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Trash2 } from "lucide-react";
 import { FormEvent, useState } from "react";
 
@@ -20,6 +20,7 @@ const ServicesList = () => {
   const [description, setDescription] = useState("");
   const [devices, setDevices] = useState("");
   const [price, setPrice] = useState(0.0);
+  const queryClient = useQueryClient();
   const { mutateAsync } = useMutation({
     mutationFn: async (data) => {
       return await fetch("http://localhost:5000/api/v1/services", {
@@ -29,6 +30,9 @@ const ServicesList = () => {
           "Content-Type": "application/json",
         },
       });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["services"] });
     },
   });
 
@@ -55,54 +59,54 @@ const ServicesList = () => {
 
   return (
     <>
-      <div className="mx-auto my-8 p-6 bg-white rounded-md shadow-md">
-        <h1 className="text-2xl font-semibold mb-4">Add Services</h1>
-        <form onSubmit={handleSubmit}>
-          <label className="block mb-4">
-            <span className="text-gray-700">Service Name:</span>
-            <input
-              type="text"
-              value={serviceName}
-              onChange={(e) => setServiceName(e.target.value)}
-              className="mt-1 p-2 border rounded-md w-full"
-            />
-          </label>
-          <label className="block mb-4">
-            <span className="text-gray-700">Description:</span>
-            <textarea
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              className="mt-1 p-2 border rounded-md w-full"
-            />
-          </label>
-          <label className="block mb-4">
-            <span className="text-gray-700">Devices (comma-separated):</span>
-            <input
-              type="text"
-              value={devices}
-              onChange={(e) => setDevices(e.target.value)}
-              className="mt-1 p-2 border rounded-md w-full"
-            />
-          </label>
-          <label className="block mb-4">
-            <span className="text-gray-700">Price:</span>
-            <input
-              type="number"
-              step="0.01"
-              value={price}
-              onChange={(e) => setPrice(parseFloat(e.target.value))}
-              className="mt-1 p-2 border rounded-md w-full"
-            />
-          </label>
-          <Button
-            type="submit"
-            className="bg-blue-500 text-white py-2 px-4 rounded-md w-full"
-          >
-            Submit Service
-          </Button>
-        </form>
-      </div>
       <Container className="mt-20 border p-0 rounded-2xl">
+        <div className="mx-auto my-8 p-6 bg-white rounded-md shadow-md">
+          <h1 className="text-2xl font-semibold mb-4">Add Services</h1>
+          <form onSubmit={handleSubmit}>
+            <label className="block mb-4">
+              <span className="text-gray-700">Service Name:</span>
+              <input
+                type="text"
+                value={serviceName}
+                onChange={(e) => setServiceName(e.target.value)}
+                className="mt-1 p-2 border rounded-md w-full"
+              />
+            </label>
+            <label className="block mb-4">
+              <span className="text-gray-700">Description:</span>
+              <textarea
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                className="mt-1 p-2 border rounded-md w-full"
+              />
+            </label>
+            <label className="block mb-4">
+              <span className="text-gray-700">Devices (comma-separated):</span>
+              <input
+                type="text"
+                value={devices}
+                onChange={(e) => setDevices(e.target.value)}
+                className="mt-1 p-2 border rounded-md w-full"
+              />
+            </label>
+            <label className="block mb-4">
+              <span className="text-gray-700">Price:</span>
+              <input
+                type="number"
+                step="0.01"
+                value={price}
+                onChange={(e) => setPrice(parseFloat(e.target.value))}
+                className="mt-1 p-2 border rounded-md w-full"
+              />
+            </label>
+            <Button
+              type="submit"
+              className="bg-blue-500 text-white py-2 px-4 rounded-md w-full"
+            >
+              Submit Service
+            </Button>
+          </form>
+        </div>
         <Table>
           <TableHeader>
             <TableRow>
